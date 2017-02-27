eae49a30efc64eba9eda3c7f6be3ec7d979b2fab
Jonathan Young <jonyoungg@gmail.com>
2017-02-27 13:54:09 -0600

    add in spec

---
+++ validate_phone.rb
@@ -0,0 +1,20 @@
+# Does a string contain a phone number?
+def has_phone_number?(string)
+end
+
+# Get a phone number back from a string
+def get_phone_number(string)
+end
+
+# Gets and returns all phone numbers from an inputed string
+def get_all_phone_numbers(string)
+end
+
+# Hide all numbers in a phone number except the last 4 digits. An example of this looks like: XXX-XXX-1234
+def hide_phone_numbers(string)
+end
+
+# Get the string of the phone number and format it for our pretend application. Ensure all of the phone numbers use dashes for delimiters.
+# Example: 312-111-2222, 312.111.2222, (312) 111-2222 would all be 312-111-2222
+def format_phone_number(string)
+end
---
+++ validate_phone_spec.rb
@@ -0,0 +1,53 @@
+require_relative 'validate_phone'
+
+describe "#has_phone_number? tests" do
+  it "returns true if it has a string that appears to be a phone number" do
+    expect(has_phone_number?("my phone number: 111-222-3333")).to eq(true)
+  end
+
+  it "returns false if it does not have a phone number" do
+    expect(has_phone_number?("please confirm your phone number: XXX-XXX-1234")).to eq(false)
+  end
+end
+
+describe "#get_phone_number tests" do
+  it "returns a phone number if the string has an phone number" do
+    expect(get_phone_number("please don't share this: 999-888-1234")).to eq("999-888-1234")
+  end
+
+  it "returns nil if it doesn't have a phone number" do
+    expect(get_phone_number("please confirm your phone number: XXX-XXX-1234")).to eq(nil)
+  end
+end
+
+describe "#get_all_phone_numbers" do
+  it "returns all phone numbers if the string has any phone numbers" do
+    expect(get_all_phone_numbers numbers("234-609-1422, 350-802-0744, 123-603-8762")).to eq(["234-609-1422", "350-802-0744", "123-603-8762"])
+  end
+
+  it "get_all_phone_numbers numbers returns an empty Array if it doesn't have any phone numbers" do
+    expect(get_all_phone_numbers numbers("please confirm your phone number: XXX-XXX-1422")).to eq([])
+  end
+end
+
+describe "#hide_phone_numbers numbers" do
+  it "obfuscates any phone numbers in the string" do
+    expect(hide_phone_numbers numbers("234-620-1422, 350-830-0744, 123-603-8762")).to eq("XXX-XXX-1422, XXX-XXX-0744, XXX-XXX-8762")
+  end
+
+  it "does not alter a string without phone numbers in it" do
+    string = "please confirm your phone number: XXX-XXX-1422"
+    expect(hide_phone_numbers numbers(string)).to eq(string)
+  end
+end
+
+describe "#format_phone_number" do
+  it "finds and reformat any phone numbers in the string" do
+    expect(format_phone_number("3112223333, 350.820.0744, 123-630-8762")).to eq("311-222-3333, 350-820-0744, 123-630-8762")
+  end
+
+  it "does not alter a string without phone numbers in it" do
+    string = "please confirm your phone number: 4421142233"
+    expect(format_phone_number(string)).to eq(string)
+  end
+end
