8cb2ec718d9c71198f9ef8baac1584e224680418
jyoung217 <jonyoungg@gmail.com>
2019-05-08 09:15:45 -0500

    added in specs

--- validate_phone.py
+++ validate_phone.py
@@ -1,23 +1,23 @@
 # Does a string contain a phone number?
-def has_phone_number(string):
+def has_phone_number(input_string):
     pass 
 
 # Get a phone number back from a string
-def get_phone_number(string):
+def get_phone_number(input_string):
     pass
 
 
 # Gets and returns all phone numbers from an inputed string
-def get_all_phone_numbers(string):
+def get_all_phone_numbers(input_string):
     pass
 
 
 # Hide all numbers in a phone number except the last 4 digits. An example of this looks like: xxx-xxx-1234
-def hide_phone_numbers(string):
+def hide_phone_numbers(input_string):
     pass
 
 
 # Get the string of the phone number and format it for our pretend application. Ensure all of the phone numbers use dashes for delimiters.
 # Example: 312-111-2222, 312.111.2222, (312) 111-2222 would all be 312-111-2222
-def format_phone_number(string):
+def format_phone_number(input_string):
     pass
--- validate_phone_spec.py
+++ validate_phone_spec.py
@@ -1,28 +1,40 @@
-from mamba import description, context, it
-from expects import expect, equal, be_true, be_false, be_none
+import unittest
 from validate_phone import *
 
-with description('#has_phone_number') as self:
-    with it('finds the correct string'):
-        expect(has_phone_number("my phone number: 111-222-3333")).to(be_true)
-        expect(has_phone_number("please confirm your phone number: XXX-XXX-1234")).to(be_false)
+class ValidatePhoneTestCases(unittest.TestCase):
+    """Tests for `validate_phone.py`."""
 
-with description('#get_phone_number') as self:
-    with it('finds the correct string'):
-        expect(get_phone_number("please don't share this: 999-888-1234")).to(equal("999-888-1234"))
-        expect(get_phone_number("please confirm your phone number: XXX-XXX-1234")).to(be_none)
+    def test_has_phone_number_true(self):
+        """When you call has_phone_number() with a legitimate phone number, you should get true"""
+        self.assertTrue(has_phone_number('my phone number: 111-222-3333'))
 
-with description('#get_all_phone_numbers') as self:
-    with it('finds the correct string'):
-        expect(get_all_phone_numbers("234-609-1422, 350-802-0744,123-603-8762")).to(equal(["234-609-1422", "350-802-0744", "123-603-8762"]))
-        expect(get_all_phone_numbers("please confirm your phone number: XXX-XXX-1422")).to(equal([]))
+    def test_has_phone_number_false(self):
+        """When you call has_phone_number() with a bad phone number, you should get false"""
+        self.assertFalse(has_phone_number('please confirm your phone number: XXX-XXX-1234'))
 
-with description('#hide_phone_number') as self:
-    with it('finds the correct string'):
-        expect(hide_phone_number("234-620-1422, 350-830-0744, 123-603-8762")).to(equal("XXX-XXX-1422, XXX-XXX-0744, XXX-XXX-8762"))
-        expect(hide_phone_number("please confirm your phone number: XXX-XXX-1422")).to(equal("please confirm your phone number: XXX-XXX-1422"))
+    def test_get_phone_number(self):
+        """When you call get_phone_number() with a good phone number, you should get the phone number"""
+        self.assertEqual(get_phone_number("please don't share this: 999-888-1234"), '999-888-1234')
 
-with description('#format_phone_number') as self:
-    with it('finds the correct string'):
-        expect(format_phone_number("3112223333, 350.820.0744, 123-630-8762")).to(equal("311-222-3333, 350-820-0744, 123-630-8762"))
-        expect(format_phone_number("please confirm your phone number: 421142233")).to(equal("please confirm your phone number: 421142233"))
+    def test_get_phone_number_none(self):
+        """When you call get_phone_number() with a bad phone number, you should get None"""
+        self.assertEqual(get_phone_number("please confirm your phone number: XXX-XXX-1234"), None)
+    
+    def test_get_all_phone_numbers(self):
+        """When you call get_all_phone_numbers(), you get all the phone numbers back"""
+        self.assertEqual(get_all_phone_numbers("234-609-1422, 350-802-0744,123-603-8762"), ["234-609-1422", "350-802-0744", "123-603-8762"])
+    
+    def test_get_all_phone_numbers_but_no_numbers(self):
+        """When you call get_all_phone_numbers() with no phone numbers, you get an empty array back"""
+        self.assertEqual(get_all_phone_numbers("please confirm your phone number: XXX-XXX-1422"), [])
+
+    def test_hide_phone_numbers(self):
+        """When you call hide_phone_numbers(), it hides all the phone numbers"""
+        self.assertEqual(hide_phone_numbers("234-620-1422, 350-830-0744, 123-603-8762"), 'XXX-XXX-1422, XXX-XXX-0744, XXX-XXX-8762')
+    
+    def test_format_phone_number(self):
+        """When you call format_phone_number(), it formats all the phone numbers"""
+        self.assertEqual(format_phone_number("3112223333, 350.820.0744, 123-630-8762"), '311-222-3333, 350-820-0744, 123-630-8762')
+
+if __name__ == '__main__':
+    unittest.main()
